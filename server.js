59ee7ba7b54581a28e12564111018ce03fc2e1e7
grasmarcussanti129 <wemmelkiarie@hotmail.com>
2025-08-31 09:59:08 -0500

    Add error handling for MongoDB connection

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ app.use(express.json());
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch(err => {
+  console.error('MongoDB connection error:', err);
 });
 
 const PORT = process.env.PORT || 5000;
