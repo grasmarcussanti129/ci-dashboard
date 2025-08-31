9ebe4625fbdbc2b23a390581aa359c094479d4f4
grasmarcussanti129 <wemmelkiarie@hotmail.com>
2025-08-31 10:01:10 -0500

    Add a middleware for logging API requests

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Middleware for logging requests
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
